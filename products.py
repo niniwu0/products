76b61a9e27d572d14656e53c3969d0ca5c406a4f
Nini Wu <ru8su60527@gmail.com>
2020-10-24 12:39:17 +0800

    add for loop

--- products.py
+++ products.py
@@ -1,3 +1,4 @@
+#二維清單 : 清單中還有清單
 products = []
 while True:
 	name = input('請輸入商品名稱: ')
@@ -6,3 +7,7 @@ while True:
 	price = input('請輸入商品價格: ')
 	products.append([name, price])
 print(products)
+
+#for loop : 一個一個取出清單中的東西
+for p in products:
+	print(p[0], '的價格是', p[1])#清單裡面的小清單位置
